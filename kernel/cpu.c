7643c86ae4cb66705e23ee7a77b59084609aa659
ZyCromerZ <neetroid97@gmail.com>
2021-09-23 05:10:55 +0700

    kernel: cpu: include linux/interrupt.h
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -33,7 +33,7 @@
 #include <linux/interrupt.h>
 #include <linux/cpuset.h>
 #include <linux/random.h>
-
+#include <linux/interrupt.h>
 #include <trace/events/power.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpuhp.h>
