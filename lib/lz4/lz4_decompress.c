e8a40bce86c87d1cd2d9a217e82c401132ab9eb6
Nick Terrell <terrelln@fb.com>
2022-01-25 12:02:45 +0530

    lz4: fix kernel decompression speed
    This patch replaces all memcpy() calls with LZ4_memcpy() which calls
    __builtin_memcpy() so the compiler can inline it.
    
    LZ4 relies heavily on memcpy() with a constant size being inlined.  In x86
    and i386 pre-boot environments memcpy() cannot be inlined because memcpy()
    doesn't get defined as __builtin_memcpy().
    
    An equivalent patch has been applied upstream so that the next import
    won't lose this change [1].
    
    I've measured the kernel decompression speed using QEMU before and after
    this patch for the x86_64 and i386 architectures.  The speed-up is about
    10x as shown below.
    
    Code	Arch	Kernel Size	Time	Speed
    v5.8	x86_64	11504832 B	148 ms	 79 MB/s
    patch	x86_64	11503872 B	 13 ms	885 MB/s
    v5.8	i386	 9621216 B	 91 ms	106 MB/s
    patch	i386	 9620224 B	 10 ms	962 MB/s
    
    I also measured the time to decompress the initramfs on x86_64, i386, and
    arm.  All three show the same decompression speed before and after, as
    expected.
    
    [1] https://github.com/lz4/lz4/pull/890
    
    Signed-off-by: Nick Terrell <terrelln@fb.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Yann Collet <yann.collet.73@gmail.com>
    Cc: Gao Xiang <gaoxiang25@huawei.com>
    Cc: Sven Schmidt <4sschmid@informatik.uni-hamburg.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Link: http://lkml.kernel.org/r/20200803194022.2966806-1-nickrterrell@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -114,6 +114,67 @@ static FORCE_INLINE int LZ4_decompress_generic(
 
 		length = token>>ML_BITS;
 
+
+		/* ip < iend before the increment */
+		assert(!endOnInput || ip <= iend);
+
+		/*
+		 * A two-stage shortcut for the most common case:
+		 * 1) If the literal length is 0..14, and there is enough
+		 * space, enter the shortcut and copy 16 bytes on behalf
+		 * of the literals (in the fast mode, only 8 bytes can be
+		 * safely copied this way).
+		 * 2) Further if the match length is 4..18, copy 18 bytes
+		 * in a similar manner; but we ensure that there's enough
+		 * space in the output for those 18 bytes earlier, upon
+		 * entering the shortcut (in other words, there is a
+		 * combined check for both stages).
+		 */
+		if ((endOnInput ? length != RUN_MASK : length <= 8)
+		   /*
+		    * strictly "less than" on input, to re-enter
+		    * the loop with at least one byte
+		    */
+		   && likely((endOnInput ? ip < shortiend : 1) &
+			     (op <= shortoend))) {
+			/* Copy the literals */
+			LZ4_memcpy(op, ip, endOnInput ? 16 : 8);
+			op += length; ip += length;
+
+			/*
+			 * The second stage:
+			 * prepare for match copying, decode full info.
+			 * If it doesn't work out, the info won't be wasted.
+			 */
+			length = token & ML_MASK; /* match length */
+			offset = LZ4_readLE16(ip);
+			ip += 2;
+			match = op - offset;
+			assert(match <= op); /* check overflow */
+
+			/* Do not deal with overlapping matches. */
+			if ((length != ML_MASK) &&
+			    (offset >= 8) &&
+			    (dict == withPrefix64k || match >= lowPrefix)) {
+				/* Copy the match. */
+				LZ4_memcpy(op + 0, match + 0, 8);
+				LZ4_memcpy(op + 8, match + 8, 8);
+				LZ4_memcpy(op + 16, match + 16, 2);
+				op += length + MINMATCH;
+				/* Both stages worked, load the next token. */
+				continue;
+			}
+
+			/*
+			 * The second stage didn't work out, but the info
+			 * is ready. Propel it right to the point of match
+			 * copying.
+			 */
+			goto _copy_match;
+		}
+
+		/* decode literal length */
+
 		if (length == RUN_MASK) {
 			unsigned int s;
 
@@ -274,6 +335,32 @@ static FORCE_INLINE int LZ4_decompress_generic(
 		/* copy match within block */
 		cpy = op + length;
 
+
+		/*
+		 * partialDecoding :
+		 * may not respect endBlock parsing restrictions
+		 */
+		assert(op <= oend);
+		if (partialDecoding &&
+		    (cpy > oend - MATCH_SAFEGUARD_DISTANCE)) {
+			size_t const mlen = min(length, (size_t)(oend - op));
+			const BYTE * const matchEnd = match + mlen;
+			BYTE * const copyEnd = op + mlen;
+
+			if (matchEnd > op) {
+				/* overlap copy */
+				while (op < copyEnd)
+					*op++ = *match++;
+			} else {
+				LZ4_memcpy(op, match, mlen);
+			}
+			op = copyEnd;
+			if (op == oend)
+				break;
+			continue;
+		}
+
+
 		if (unlikely(offset < 8)) {
 			const int dec64 = dec64table[offset];
 
@@ -281,9 +368,15 @@ static FORCE_INLINE int LZ4_decompress_generic(
 			op[1] = match[1];
 			op[2] = match[2];
 			op[3] = match[3];
+
 			match += dec32table[offset];
 			memcpy(op + 4, match, 4);
 			match -= dec64;
+
+			match += inc32table[offset];
+			LZ4_memcpy(op + 4, match, 4);
+			match -= dec64table[offset];
+
 		} else {
 			LZ4_copy8(op, match);
 			match += 8;
